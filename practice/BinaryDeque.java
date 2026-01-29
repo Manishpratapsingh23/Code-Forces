4d095496e216ecaa2df5dff6960e1bbc0b219b25
Manishpratapsingh23 <manishpratap2205@gmail.com>
2026-01-29 19:59:33 +0530

    update

---
+++ practice/BinaryDeque.java
@@ -0,0 +1,112 @@
+import java.util.*;
+
+public class BinaryDeque {
+
+    // prime check TC: O(underroot N)
+    private static boolean checkPrime(int n){
+        int count = 0;
+        for(int i=1;i*i<=n;i++){
+            if(n%i==0){
+                count++;
+                if((n/i)!=i) count++;
+            }
+            if(count>2) return false;
+        }
+        return true;
+    }
+
+    private static int GCD(int a, int b){
+        while(a>0 && b>0){
+            if(a>b) a = a%b;
+            else b = b%a;
+        }
+        if(a==0) return b;
+        return a;
+    }
+
+    // Prime Factors TC: O(underroot N)
+    private static List<Integer> primeFactors(int n){
+        List<Integer> factors = new ArrayList<>();
+        for(int i=2;i<Math.sqrt(n);i++){
+            if(n%i==0){
+                factors.add(i);
+                while(n%i==0) n = n/i;
+            }
+        }
+        if(n!=1) factors.add(n);
+        return factors;
+    }
+
+    // Sieve of Eratosthenes
+    // TC: n*log(log(n))
+    // SC: O(n)
+    private static int[] SieveEratosthenes(int n){
+        int prime[] = new int[n+1];
+        Arrays.fill(prime, 1);
+        prime[0]=0;
+        prime[1]=0;
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(prime[i]==1){
+                for(int j=i*i;j<=n;j+=i){
+                    prime[j]=0;
+                }
+            }
+        }
+        return prime;
+    }
+
+    // Smallest Prime Factors
+    private static List<Integer> SPF(int n){
+        List<Integer> lst = new ArrayList<>();
+        int spf[] = SOE(100000);
+        while(n!=1){
+            lst.add(spf[n]);
+            n = n/spf[n];
+        }
+        return lst;
+    }
+
+    private static int[] SOE(int n){
+        int spf[] = new int[(int) (n+1)];
+        for(int i=1;i<=n;i++) spf[i]=i;
+        for(int i=2;i*i<=n;i++){
+            if(spf[i]==i){
+                for(int j=i*i;j<=n;j+=i){
+                    if(spf[j]==j){
+                        spf[j] = i;
+                    }
+                }
+            }
+        }
+        return spf;
+    }
+
+    private static void solve_kro(Scanner sc){
+        int n = sc.nextInt();
+        int s = sc.nextInt();
+        //Deque<Integer> queue = new ArrayDeque<>();
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0,-1);
+        int ans = -1;
+        for(int i=0;i<n;i++){
+
+        	int x = sc.nextInt();
+            sum+=x;
+            if(map.containsKey(sum-s)){
+                ans = Math.max(ans, i-map.get(sum-s));
+            }
+            if(!map.containsKey(sum)) map.put(sum, i);
+        }
+        System.out.println(ans==-1 ? -1 : n-ans);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        while (test-- > 0) {
+            solve_kro(sc);
+        }
+    }
+}
---
+++ practice/MirrorGrid.java
@@ -0,0 +1,119 @@
+import java.util.*;
+
+public class MirrorGrid {
+
+    // prime check TC: O(underroot N)
+    private static boolean checkPrime(int n){
+        int count = 0;
+        for(int i=1;i*i<=n;i++){
+            if(n%i==0){
+                count++;
+                if((n/i)!=i) count++;
+            }
+            if(count>2) return false;
+        }
+        return true;
+    }
+
+    private static int GCD(int a, int b){
+        while(a>0 && b>0){
+            if(a>b) a = a%b;
+            else b = b%a;
+        }
+        if(a==0) return b;
+        return a;
+    }
+
+    // Prime Factors TC: O(underroot N)
+    private static List<Integer> primeFactors(int n){
+        List<Integer> factors = new ArrayList<>();
+        for(int i=2;i<Math.sqrt(n);i++){
+            if(n%i==0){
+                factors.add(i);
+                while(n%i==0) n = n/i;
+            }
+        }
+        if(n!=1) factors.add(n);
+        return factors;
+    }
+
+    // Sieve of Eratosthenes
+    // TC: n*log(log(n))
+    // SC: O(n)
+    private static int[] SieveEratosthenes(int n){
+        int prime[] = new int[n+1];
+        Arrays.fill(prime, 1);
+        prime[0]=0;
+        prime[1]=0;
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(prime[i]==1){
+                for(int j=i*i;j<=n;j+=i){
+                    prime[j]=0;
+                }
+            }
+        }
+        return prime;
+    }
+
+    // Smallest Prime Factors
+    private static List<Integer> SPF(int n){
+        List<Integer> lst = new ArrayList<>();
+        int spf[] = SOE(100000);
+        while(n!=1){
+            lst.add(spf[n]);
+            n = n/spf[n];
+        }
+        return lst;
+    }
+
+    private static int[] SOE(int n){
+        int spf[] = new int[(int) (n+1)];
+        for(int i=1;i<=n;i++) spf[i]=i;
+        for(int i=2;i*i<=n;i++){
+            if(spf[i]==i){
+                for(int j=i*i;j<=n;j+=i){
+                    if(spf[j]==j){
+                        spf[j] = i;
+                    }
+                }
+            }
+        }
+        return spf;
+    }
+
+    private static void solve_kro(Scanner sc){
+        int n = sc.nextInt();
+        int arr[][] = new int[n][n];
+        for(int i=0;i<n;i++){
+        	String s = sc.next();
+        	for(int j=0;j<n;j++){
+        		arr[i][j] = s.charAt(j)-'0';
+                //System.out.print(arr[i][j]+" ");
+        	}
+            //System.out.println();
+        }
+        int ans = 0;
+        int nn = n-1;
+
+        for (int i = 0; i < (n + 1) / 2; i++) {
+    		for (int j = 0; j < n / 2; j++) {
+		        int sum = 0;
+		        sum += arr[i][j];
+		        sum += arr[j][nn - i];
+		        sum += arr[nn - j][i];
+		        sum += arr[nn - i][nn - j];
+
+		        ans += Math.min(sum, 4 - sum);
+		    }
+		}
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        while (test-- > 0) {
+            solve_kro(sc);
+        }
+    }
+}
